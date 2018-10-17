9a66511d653854407b6228eeb2a8449b22a4ff3e
xunqyzhao <xunqyzhao@tencent.com>
2018-10-17 12:52:58 +0800

    clear previous rule before add new rule

--- cmd/rule.go
+++ cmd/rule.go
@@ -21,20 +21,31 @@ func containsNoSuchRule(err error) bool {
 }
 
 func ensureRule(cidr *net.IPNet) error {
+	err := clearPreviousRule()
+	if err != nil {
+		return err
+	}
+
 	rule := netlink.NewRule()
 	rule.Dst = cidr
 	rule.Table = mainRouteTable
 	rule.Priority = cidrRulePriority
 
-	err := netlink.RuleDel(rule)
-	if err != nil && !containsNoSuchRule(err) {
-		return errors.Wrapf(err, "add cidr rule: failed to delete old rule for %v", *cidr)
-	}
-
 	err = netlink.RuleAdd(rule)
 	if err != nil {
 		return errors.Wrapf(err, "add cidr rule: failed to add rule for %v", *cidr)
 	}
+	return nil
+}
 
+func clearPreviousRule() error {
+	rule := netlink.NewRule()
+	rule.Table = mainRouteTable
+	rule.Priority = cidrRulePriority
+
+	err := netlink.RuleDel(rule)
+	if err != nil && !containsNoSuchRule(err) {
+		return errors.Wrapf(err, "clear previous rule: failed to delete old rule",)
+	}
 	return nil
 }
