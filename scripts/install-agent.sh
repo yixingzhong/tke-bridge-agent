04d6035317887185a888e79e118f62f84d5b3b6c
xunqyzhao <xunqyzhao@tencent.com>
2019-01-07 17:30:01 +0800

    no cni plugins copy

--- scripts/install-agent.sh
+++ scripts/install-agent.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
-echo "=====Starting installing cni ========="
-tar xzf /cni-plugins.tgz -C /host/opt/cni/bin/
 echo "=====Starting tke-bridge-agent ==========="
 /tke-bridge-agent
